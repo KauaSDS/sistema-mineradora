0642c30e267237b2be48e48deea5cb8b8a126fe9
Your Name <you@example.com>
2026-06-25 22:23:43 -0300

    Corrigindo erros

--- teste-react/src/App.jsx
+++ teste-react/src/App.jsx
@@ -1,93 +1,26 @@
-import {useState} from "react"
+import { useState } from "react"
 import Menu from "./components/Menu"
-import Equipamento from "./components/Equipamento"
+import Inicio from "./pages/Inicio"
+import Equipamentos from "./pages/Equipamentos"
+import Cidades from "./pages/Cidades"
+import Funcionarios from "./pages/Funcionarios"
+import Servicos from "./pages/Servicos"
 
-function App(){
-const [pagina,setPagina] = useState("inicio")    
-const [nome,setNome] = useState("")
-const [setor,setSetor] = useState("")
-const [equipamentos,setEquipamentos] = useState([])
+function App() {
+  const [pagina, setPagina] = useState("inicio")
 
-function cadastrar(){
-const novoEquipamento = {
-    nome: nome, 
-    setor:setor
-}
-setEquipamentos([...equipamentos,novoEquipamento])
-setNome("")
-setSetor("")
-alert("O equipamento foi cadastrado com sucesso!")
-}
-
-
-
-return (
+  return (
     <div>
+      <Menu setPagina={setPagina} />
+      <hr />
 
-        <Menu mudarPagina={setPagina}/>
-        <hr/>
-
-        {
-            pagina === "inicio" && (
-                <div>
-                    <h1> Sistema de Controle de Equipamentos</h1>
-                    <p>
-                        Bem-vindo ao sistema da mineradora.
-                    </p>
-                    <p>Utilize o menu para navegar.</p>
-                </div>
-            )
-        }
-
-        {
-            pagina === "cadastro" && (
-                <div>
-            <h1>Cadastro de Equipamentos</h1>
-        <input
-        type="text"
-        placeholder="Nome do Equipamento"
-        value={nome}
-        onChange={(e) => setNome(e.target.value)}
-        />
-    <br/><br/>
-        <input
-        type="text"
-        placeholder="Setor do equipamento"
-        value={setor}
-        onChange={(e) => setSetor(e.target.value)}
-        />
-<br /><br />
-<button onClick={cadastrar}> Cadastrar </button>
-                </div>
-            )
-        }
-
-{
-    pagina === "relatorio" && (
-        <div>
-            <h2> Relatório de Equipamentos </h2>
-            <p>
-                Total de Equipamentos cadastrados:
-                {equipamentos.length}
-            </p>
-                {
-    equipamentos.map((equipamento,index) => (
-        <Equipamento
-        key={index}
-        nome={equipamento.nome}
-        setor={equipamento.setor}
-        />
-))
-}
-        </div>
-    )
-}
-        
-
-
-
+      {pagina === "inicio" && <Inicio />}
+      {pagina === "equipamentos" && <Equipamentos />}
+      {pagina === "cidades" && <Cidades />}
+      {pagina === "funcionarios" && <Funcionarios />}
+      {pagina === "servicos" && <Servicos />}
     </div>
-)
-
+  )
 }
+
 export default App
